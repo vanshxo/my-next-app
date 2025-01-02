cf96ce9a3d8db6392212def734610cbc989fef26
vanshxo <khatrivansh43@gmail.com>
2025-01-03 01:53:29 +0530

    check2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button } from "@/components/ui/button"
+
 import Link from "next/link"
 import { motion } from "framer-motion"
 import { useRouter } from 'next/navigation'
